4d60ff70f1906e6a68945a22359c7ebc1540d4af
Chairul Aziz <m.chairul669@gmail.com>
2019-03-17 14:01:51 +0700

    Web Assets

---
+++ public/assets/js/custom_front.js
@@ -0,0 +1,46 @@
+AOS.init();
+
+$(document).ready(function() {
+  let prevScrollPos = window.pageYOffset;
+  
+  if (prevScrollPos == 0) {
+    $('#navbar').css('top', '0px').removeClass('bg-purple').addClass('bg-transparent');
+  }
+
+  if (prevScrollPos > 500) {
+    $('.back-to-top').removeClass('invisible').addClass('visible');
+  }
+
+  $(window).on('scroll', function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+      if (currentScrollPos == 0) {
+        $('#navbar').css('top', '0px').removeClass('bg-purple').addClass('bg-transparent');
+      } else {
+        $('#navbar').css('top', '0px').removeClass('bg-transparent').addClass('bg-purple');
+      }
+    } else {
+      $('#navbar').css('top', '-100px');
+    }
+    prevScrollPos = currentScrollPos;
+
+    if (prevScrollPos > 300) {
+      $('.back-to-top').removeClass('invisible').addClass('visible');
+    } else {
+      $('.back-to-top').removeClass('visible').addClass('invisible');
+    }
+  });
+
+  $('.sliders').DrSlider({
+    navigationType: 'circle',
+    showProgress: false,
+    positionNavigation: 'in-center-bottom',
+    navigationColor: '#ffffff',
+    navigationHighlightColor: '#7800bc',
+    navigationHoverColor: '#7800bc'
+  });
+
+  $('.navbar-toggler').click(function() {
+    $('#navbar').toggleClass('bg-purple');
+  });
+})
