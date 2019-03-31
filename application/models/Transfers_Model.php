56007eddb97c0a08fb5159c8f19bc0ebbdb5aecd
Chairul Aziz <m.chairul669@gmail.com>
2019-03-31 10:50:58 +0700

    Manage Transfer

--- application/models/Transfers_Model.php
+++ application/models/Transfers_Model.php
@@ -5,7 +5,7 @@ class Transfers_Model extends CI_Model
 {
     public function insert_new_transfer($data_transfer)
     {
-        $result = $this->db->insert('tranfers', $data_transfer);
+        $result = $this->db->insert('transfers', $data_transfer);
         return $result;
     }
 }
